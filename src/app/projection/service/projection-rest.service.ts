72a44267c42c6985bfaac96d174670b98080c469
Pedro Lemos <pedroemlemos@gmail.com>
2020-10-23 22:22:18 -0300

    Create projection service and rest service

--- src/app/projection/service/projection-rest.service.ts
+++ src/app/projection/service/projection-rest.service.ts
@@ -1,9 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { ProjectionResponse } from '../integration/projection.response';
 
 @Injectable()
 export class ProjectionRestService {
 
   constructor(private readonly http: HttpClient) { }
 
+  public getProjection(ticker: string): Observable<ProjectionResponse> {
+    const params = new HttpParams().append('ticker', ticker);
+
+    return this.http.get<ProjectionResponse>('/api/projections', { params });
+  }
 }
--- src/app/projection/service/projection.service.ts
+++ src/app/projection/service/projection.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ProjectionModel } from '../model/projection.model';
 import { ProjectionRestService } from './projection-rest.service';
 
 @Injectable()
@@ -7,4 +11,9 @@ export class ProjectionService {
 
   constructor(private readonly rest: ProjectionRestService) { }
 
+  public priceForecast(ticker: string): Observable<ProjectionModel> {
+    return this.rest.getProjection(ticker).pipe(
+      map(response => new ProjectionModel(response)),
+    );
+  }
 }
