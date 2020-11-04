c30a6e666784ffd10586f257cffda00b1ca27384
Pedro Lemos <pedroemlemos@gmail.com>
2020-11-03 22:49:59 -0300

    Set pt-br locale data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localept from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -13,6 +15,8 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+
+registerLocaleData(localept, 'pt');
 
 @NgModule({
   declarations: [
@@ -30,6 +34,7 @@ import { MainNavComponent } from './main-nav/main-nav.component';
   providers: [
     StatusBar,
     SplashScreen,
+    { provide: LOCALE_ID, useValue: 'pt' },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
