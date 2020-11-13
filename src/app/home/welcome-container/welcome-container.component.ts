ed975ab05dc2a994da9dd62c70042529d96c49bb
Pedro Lemos <pedroemlemos@gmail.com>
2020-11-12 23:13:16 -0300

    Add content to home page

--- src/app/home/welcome-container/welcome-container.component.ts
+++ src/app/home/welcome-container/welcome-container.component.ts
@@ -5,4 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './welcome-container.component.html',
   styleUrls: ['./welcome-container.component.scss'],
 })
-export class WelcomeContainerComponent { }
+export class WelcomeContainerComponent {
+  public readonly routes: Array<{
+    link: string;
+    title: string;
+    subtitle?: string;
+    description?: string;
+    icon: string;
+  }>;
+
+  constructor() {
+    this.routes = [{
+      link: '/indicadores',
+      title: 'Indicadores',
+      subtitle: 'Consulta de',
+      description: 'Os principais indicadores fundamentalistas para avaliar a saúde financeira de qualquer empresa para montar uma carteira com boas ações.',
+      icon: 'stats-chart-outline'
+    }, {
+      link: '/vencedoras',
+      title: 'Vencedoras',
+      subtitle: 'Ações de Empresas',
+      description: 'Saiba quais são as empresas vencedoras atualmente através de uma versão refinada do método Bazin.',
+      icon: 'podium-outline'
+    }, {
+      link: '/cotacao',
+      title: 'Cotação',
+      subtitle: 'Histórico de',
+      description: 'Consulte todo histórico de cotações de qualquer empresa e avalie faixas de preço para realizar suas operações de compra e venda.',
+      icon: 'pulse-outline'
+    }, {
+      link: '/projecao',
+      title: 'Projeção',
+      subtitle: 'Cotação Futura',
+      description: 'Veja uma projeção de cotações futuras de uma ação para os próximos pregões e avalie a possibilidade de se posicionar hoje.',
+      icon: 'trending-up-outline'
+    }];
+  }
+}
